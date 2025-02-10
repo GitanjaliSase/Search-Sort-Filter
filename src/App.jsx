30a62de8620ec44b4678a1cff687fa513db8f708
GitanjaliSase <sasegitanjali20@gmail.com>
2025-02-11 00:40:15 +0530

    filter add for age and city

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { MapPin as MapPinIcon ,Cake as CakeIcon } from "lucide-react";
 export default function App(){
   const [searchText, setSearchText]=useState("");
   const [filterUsers,setFilterUsers]=useState(USERS);
+  const [filterCity,setFilterCity]=useState("");
+  const [filterAge,setFilterAge]=useState("");
+
    useEffect(()=>{
     if(!searchText){
       setFilterUsers(USERS);
@@ -26,16 +29,68 @@ export default function App(){
     setFilterUsers(tempFilteredUsers);
    },[searchText]);
 
+   useEffect(()=>{
+    if(!filterAge && !filterCity){
+      setFilterUsers(USERS);
+      return;
+    }
+    const tempFilteredUsers=USERS.filter((user)=>{
+      if(filterCity && user.city===filterCity && filterAge && user.age===parseInt(filterAge)){
+        return true;
+      }
+      if(filterAge && !filterCity && user.age===parseInt(filterAge)){
+        return true;
+      }
+      if(!filterAge && filterCity && user.city===filterCity){
+        return true;
+      }
+      return false;
+    });
+    setFilterUsers(tempFilteredUsers);
+   },[filterCity,filterAge]);
+
     return(
      <div className="bg-slate-100 min-h-screen">
       <h1 className="text-center text-lime-400 text-4xl font-bolf py-5"> Search ,Sort, Filter</h1>
        
-       <input type="text" placeholder="Search" className="w-2/3 p-2 mb-10 my-10 bg-white block mx-auto rounded-lg text-2xl focus:outline-none border border-gray-200 "
+       <input type="text" placeholder="Search" className="w-2/3 p-2 mt-10 my-10 bg-white block mx-auto rounded-lg text-2xl focus:outline-none border border-gray-200 "
          value={searchText}
-         onChange={(e)=>setSearchText(e.target.value)}
+         onChange={(e)=>setSearchText(e.target.value.toLocaleLowerCase())}
        />
+      { searchText ? <p className="text-center  mt-2">
+         {filterUsers.length===0 ? "Oops ! No users found for search result... Try another search...":
+         'Found {filterUsers.length} users for search results../'}
+         </p>:
+       null}
 
-       <div className="flex flex-wrap justify-around">{USERS.map((userData,index)=>{
+       <div>
+         <div>
+            <span>Filter By City:</span>
+            <select className="bg-white text-lg my-2 rounded-lg px-5" value={filterCity}
+            onChange={(e)=>setFilterCity(e.target.value)}>
+              <option value="">All</option>
+              {
+                USERS.map((user)=>{
+                  return <option key={user.city} value={user.city}>{user.city}</option>
+                })
+              }
+            </select>
+         </div>
+         <div>
+         <span>Filter By Age:</span>
+         <select className="bg-white text-lg my-2 rounded-lg px-5" value={filterAge}
+            onChange={(e)=>setFilterAge(e.target.value)}>
+              <option value="">All</option>
+              {
+                USERS.map((user)=>{
+                  return <option key={user.age} value={user.age}>{user.age}</option>
+                })
+              }
+            </select>
+         </div>
+       </div>
+
+       <div className="flex flex-wrap justify-around mt-10">{USERS.map((userData,index)=>{
           const {name,city,age,avatar}=userData;
           
           return(
